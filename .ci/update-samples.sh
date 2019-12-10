ee375316d7f08467f351e3a293d43c0ee92ce3f6
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-12-10 18:52:55 +0530

    Update plugin version in client-samples after release (#70)

---
+++ .ci/update-samples.sh
@@ -0,0 +1,14 @@
+export SAMPLES_REPO_SLUG=gluonhq/client-samples
+
+cd $TRAVIS_BUILD_DIR
+git clone https://github.com/$SAMPLES_REPO_SLUG
+cd client-samples/Maven
+
+# Update plugin version
+mvn -f HelloFX versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
+mvn -f HelloFXML versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
+mvn -f HelloGluon versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
+mvn -f HelloWorld versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
+
+git commit */pom.xml -m "Upgrade client-maven-plugin version to $newVersion" --author "Gluon Bot <githubbot@gluonhq.com>"
+git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$SAMPLES_REPO_SLUG HEAD:master
