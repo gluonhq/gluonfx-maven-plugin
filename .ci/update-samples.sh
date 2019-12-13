36f542d7a2fd6b66dce99ad1924d1d735532fabe
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-12-13 12:25:50 +0530

    Fix variable name containing the new version (#72)

--- .ci/update-samples.sh
+++ .ci/update-samples.sh
@@ -10,5 +10,5 @@ mvn -f HelloFXML versions:set-property -Dproperty=client.plugin.version -DnewVer
 mvn -f HelloGluon versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
 mvn -f HelloWorld versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
 
-git commit */pom.xml -m "Upgrade client-maven-plugin version to $newVersion" --author "Gluon Bot <githubbot@gluonhq.com>"
+git commit */pom.xml -m "Upgrade client-maven-plugin version to $1" --author "Gluon Bot <githubbot@gluonhq.com>"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$SAMPLES_REPO_SLUG HEAD:master
