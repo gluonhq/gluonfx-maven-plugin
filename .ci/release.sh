31a69bc4e8833ecc81dd280935eb4c4f3c44ba72
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-12-13 13:18:40 +0530

    README should use the latest release version

--- .ci/release.sh
+++ .ci/release.sh
@@ -6,13 +6,17 @@ cp .travis.settings.xml $HOME/.m2/settings.xml && mvn deploy
 # Update version by 1
 newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
 
+# Update README with the latest released version
+sed -i "0,/<version>.*<\/version>/s//<version>$TRAVIS_TAG<\/version>/" README.md
+git commit README.md -m "Use latest release v$TRAVIS_TAG in README" --author "Github Bot <githubbot@gluonhq.com>"
+
 # Update project version to next snapshot version
 mvn versions:set -DnewVersion=$newVersion-SNAPSHOT -DgenerateBackupPoms=false
 
 # Update Substrate to next snapshot version
 mvn versions:use-next-snapshots -Dincludes=com.gluonhq:substrate -DgenerateBackupPoms=false
 
-git commit pom.xml -m "Prepare development of $newVersion" --author "Gluon Bot <githubbot@gluonhq.com>"
+git commit pom.xml -m "Prepare development of v$newVersion" --author "Gluon Bot <githubbot@gluonhq.com>"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$TRAVIS_REPO_SLUG HEAD:master
 
 # Update samples
