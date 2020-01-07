08ca9b7ef9fa7f5fd48c5da9fc8d89956b7dd936
jose.pereda <jose.pereda@gluonhq.com>
2020-01-07 11:16:50 +0100

    #94 Create package goal

---
+++ src/main/java/com/gluonhq/NativePackageMojo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020, Gluon
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.gluonhq;
+
+import com.gluonhq.substrate.SubstrateDispatcher;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.nio.file.Path;
+
+@Mojo(name = "package")
+public class NativePackageMojo extends NativeBaseMojo {
+
+    public void execute() throws MojoExecutionException {
+        super.execute();
+
+        try {
+            Path client = outputDir.toPath();
+            getLog().debug("Start packaging in " + client.toString());
+
+            SubstrateDispatcher dispatcher = new SubstrateDispatcher(client, clientConfig);
+            dispatcher.nativePackage();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new MojoExecutionException("Error", e);
+        }
+    }
+
+}
