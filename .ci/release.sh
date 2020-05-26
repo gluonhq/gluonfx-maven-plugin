5b351493fe2c64d1fddc07ce2a1f26730daf8115
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-05-26 13:19:49 +0530

    Find and update next Substrate version from current version

--- .ci/release.sh
+++ .ci/release.sh
@@ -18,18 +18,22 @@ tar xvf gpg_keys.tar
 # Release artifacts
 cp .travis.settings.xml $HOME/.m2/settings.xml && mvn deploy -DskipTests=true -B -U -Prelease
 
-# Update version by 1
-newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
-
 # Update README with the latest released version
 sed -i "0,/<version>.*<\/version>/s//<version>$TRAVIS_TAG<\/version>/" README.md
 git commit README.md -m "Use latest release v$TRAVIS_TAG in README"
 
+# Update Substrate version
+SUBSTRATE_VERSION=$(mvn help:evaluate -Dexpression=substrate.version -q -DforceStdout)
+NEW_SUBSTRATE_VERSION=${SUBSTRATE_VERSION%.*}.$((${SUBSTRATE_VERSION##*.} + 1))
+
+# Update version by 1
+NEW_PROJECT_VERSION=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
+
 # Update project version to next snapshot version
-mvn versions:set -DnewVersion=$newVersion-SNAPSHOT -DgenerateBackupPoms=false
+mvn versions:set -DnewVersion=$NEW_PROJECT_VERSION-SNAPSHOT -DgenerateBackupPoms=false
 
 # Update Substrate to next snapshot version
-mvn versions:use-next-snapshots -Dincludes=com.gluonhq:substrate -DgenerateBackupPoms=false
+mvn versions:set-property -Dproperty=substrate.version -DnewVersion=$NEW_SUBSTRATE_VERSION-SNAPSHOT -DgenerateBackupPoms=false
 
 git commit pom.xml -m "Prepare development of $newVersion"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$TRAVIS_REPO_SLUG HEAD:master
