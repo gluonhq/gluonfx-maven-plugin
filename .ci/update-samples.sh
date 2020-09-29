9fc2336dc22d387d8281c6a21143256a7957bc14
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-09-29 21:53:11 +0530

    client-samples is deprecated. Update gluon-samples instead. (#262)

--- .ci/update-samples.sh
+++ .ci/update-samples.sh
@@ -1,14 +1,11 @@
-SAMPLES_REPO_SLUG=gluonhq/client-samples
+SAMPLES_REPO_SLUG=gluonhq/gluon-samples
 
 cd $TRAVIS_BUILD_DIR
 git clone https://github.com/$SAMPLES_REPO_SLUG
-cd client-samples/Maven
+cd gluon-samples
 
 # Update plugin version
-mvn -f HelloFX versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
-mvn -f HelloFXML versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
-mvn -f HelloGluon versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
-mvn -f HelloWorld versions:set-property -Dproperty=client.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
+mvn versions:set-property -Dproperty=client.maven.plugin.version -DnewVersion="$1" -DgenerateBackupPoms=false
 
-git commit */pom.xml -m "Update client-maven-plugin version to $1"
+git commit pom.xml -m "Update client-maven-plugin version to $1"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$SAMPLES_REPO_SLUG HEAD:master
