16fa46e88b6f9fadf391f539fb5096fc6b8103fd
jose.pereda <jose.pereda@gluonhq.com>
2020-11-13 16:13:19 +0100

    remove change of unused property

--- .ci/update-docs.sh
+++ .ci/update-docs.sh
@@ -7,7 +7,6 @@ git clone https://gluon-bot:$GITHUB_PASSWORD@github.com/$DOCS_REPO_SLUG
 cd docs
 
 # Update properties
-sed -i "s/CLIENT_VERSION=.*/CLIENT_VERSION=$1/g" gradle.properties
 sed -i "s/CLIENT_MAVEN_PLUGIN_VERSION=.*/CLIENT_MAVEN_PLUGIN_VERSION=$1/g" gradle.properties
 sed -i "s/CLIENT_GRADLE_PLUGIN_VERSION=.*/CLIENT_GRADLE_PLUGIN_VERSION=$1/g" gradle.properties
 
