04eff5efe1ebdbe0066d0b506aa7ffb9a5b0cb3f
jose.pereda <jose.pereda@gluonhq.com>
2020-11-13 16:09:05 +0100

    Update client version properties separately

--- .ci/update-docs.sh
+++ .ci/update-docs.sh
@@ -7,7 +7,9 @@ git clone https://gluon-bot:$GITHUB_PASSWORD@github.com/$DOCS_REPO_SLUG
 cd docs
 
 # Update properties
-sed -i "s/CLIENT\(.*\)_VERSION=.*/CLIENT\1_VERSION=$1/g" gradle.properties
+sed -i "s/CLIENT_VERSION=.*/CLIENT_VERSION=$1/g" gradle.properties
+sed -i "s/CLIENT_MAVEN_PLUGIN_VERSION=.*/CLIENT_MAVEN_PLUGIN_VERSION=$1/g" gradle.properties
+sed -i "s/CLIENT_GRADLE_PLUGIN_VERSION=.*/CLIENT_GRADLE_PLUGIN_VERSION=$1/g" gradle.properties
 
 git commit gradle.properties -m "Update client-maven-plugin version to $1"
 git push https://gluon-bot:$GITHUB_PASSWORD@github.com/$DOCS_REPO_SLUG HEAD:master
