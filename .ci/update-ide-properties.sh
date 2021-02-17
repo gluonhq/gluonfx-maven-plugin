76f02d5805c45f357013d2a0cad3f418d8a8c4aa
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-02-17 22:55:38 +0530

    Install awscli using Travis addons (#305)

--- .ci/update-ide-properties.sh
+++ .ci/update-ide-properties.sh
@@ -1,6 +1,3 @@
-# Install AWS CLI
-sudo apt-get install awscli
-
 # Update settings property file
 aws s3 cp s3://download.gluonhq.com/ideplugins/settings-2.10.properties /tmp
 sed -i "s/clientMavenPlugin=.*/clientMavenPlugin=$1/g" /tmp/settings-2.10.properties
