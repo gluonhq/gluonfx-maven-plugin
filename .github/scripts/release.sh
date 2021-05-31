8cecdebd5207a10878a2fab849e028c5d143442c
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-05-31 21:39:41 +0530

    Fix scripts

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -8,16 +8,16 @@ git config --global user.name "Gluon Bot"
 git config --global user.email "githubbot@gluonhq.com"
 
 echo "Update samples"
-sh .ci/update-samples.sh "$1"
+sh $GITHUB_WORKSPACE/.github/scripts/update-samples.sh "$1"
 
 echo "Update hello-gluon-ci"
-sh .ci/update-hello-gluon-ci.sh "$1"
+sh $GITHUB_WORKSPACE/.github/scripts/update-hello-gluon-ci.sh "$1"
 
 echo "Update archetypes"
-bash .ci/update-archetypes.sh "$1"
+bash $GITHUB_WORKSPACE/.github/scripts/update-archetypes.sh "$1"
 
 echo "Update docs"
-bash .ci/update-docs.sh "$1"
+bash $GITHUB_WORKSPACE/.github/scripts/update-docs.sh "$1"
 
 echo "Update ide-plugin properties"
-bash .ci/update-ide-properties.sh "$1"
+bash $GITHUB_WORKSPACE/.github/scripts/update-ide-properties.sh "$1"
