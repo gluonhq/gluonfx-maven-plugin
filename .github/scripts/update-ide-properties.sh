0d09d444dcc6712ba3b7a2030815bcf27bc599b2
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-11-01 12:12:03 +0530

    Update location for settings (#381)

--- .github/scripts/update-ide-properties.sh
+++ .github/scripts/update-ide-properties.sh
@@ -1,4 +1,4 @@
 # Update settings property file
-aws s3 cp s3://download.gluonhq.com/ideplugins/settings-2.10.properties /tmp --region us-east-1 --debug
-sed -i "s/clientMavenPlugin=.*/clientMavenPlugin=$TAG/g" /tmp/settings-2.10.properties
-aws s3 cp /tmp/settings-2.10.properties s3://download.gluonhq.com/ideplugins/ --acl public-read --region us-east-1 --debug
+aws s3 cp s3://download.gluonhq.com/ideplugins/settings-2.10.4.properties /tmp --region us-east-1 --debug
+sed -i "s/gluonfxMavenPlugin=.*/gluonfxMavenPlugin=$TAG/g" /tmp/settings-2.10.4.properties
+aws s3 cp /tmp/settings-2.10.4.properties s3://download.gluonhq.com/ideplugins/ --acl public-read --region us-east-1 --debug
